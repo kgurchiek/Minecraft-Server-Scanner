419db6848e4bd92a486ad973347f82e42797f521
kgurchiek <kgurchiek1@gmail.com>
2023-05-22 23:35:43 -0700

    set packet rate

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { spawn } = require('child_process');
 
 async function fullPort(port) {
-  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan.json`]);
+  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=100000 --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan.json`]);
 
   childProcess.stdout.on('data', (data) => {
     // Process the output as needed
