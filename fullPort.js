03bdb46a12a6c9a60a2fbc55119de638882e8c28
kgurchiek <kgurchiek1@gmail.com>
2023-05-26 11:25:07 -0700

    separate files for scan functions

---
+++ fullPort.js
@@ -0,0 +1,25 @@
+async function fullPort(port) {
+  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=100000 --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan.json`]);
+
+  childProcess.stdout.on('data', (data) => {
+    // Process the output as needed
+    console.log(data.toString());
+  });
+
+  childProcess.stderr.on('data', (data) => {
+    // Handle any error output
+    console.error(data.toString());
+  });
+
+  childProcess.on('close', async (code) => {
+    if (code === 0) {
+      console.log('Masscan finished.');
+      await save();
+      knownIps();
+    } else {
+      console.error(`Command exited with code ${code}`);
+    }
+  });
+}
+
+fullPort(25500);
