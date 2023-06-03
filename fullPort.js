e560984bba23e5b2e80ece450e71bea760391750
kgurchiek <kgurchiek1@gmail.com>
2023-06-03 06:53:59 -0700

    Use console logs instead of json saving

--- fullPort.js
+++ fullPort.js
@@ -2,11 +2,30 @@ const fs = require('fs');
 const { spawn } = require('child_process');
 
 async function fullPort(port) {
-  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan.json`]);
+  const writeStream = fs.createWriteStream('./ips1');
+  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ -`]);
 
   childProcess.stdout.on('data', (data) => {
     // Process the output as needed
-    console.log(data.toString());
+    if (line.startsWith('{')) {
+      const obj = JSON.parse(line);
+      for (const port of obj.ports) {
+        if (port.reason !== "syn-ack") {
+          const splitIP = obj.ip.split('.');
+          const buffer = Buffer.from([
+            parseInt(splitIP[0]),
+            parseInt(splitIP[1]),
+            parseInt(splitIP[2]),
+            parseInt(splitIP[3]),
+            Math.floor(port.port / 256),
+            port.port % 256
+          ]);
+          writeStream.write(buffer);
+        }
+      }
+    } else {
+      console.log(data.toString());
+    }
   });
 
   childProcess.stderr.on('data', (data) => {
@@ -17,7 +36,7 @@ async function fullPort(port) {
   childProcess.on('close', async (code) => {
     if (code === 0) {
       console.log('Masscan finished.');
-      //await save();
+      writeStream.end();
       //knownIps();
     } else {
       console.error(`Command exited with code ${code}`);
--- index.js
+++ index.js
@@ -7,11 +7,30 @@ const scannedServers = client.db("MCSS").collection("scannedServers");
 const config = require('./config.json');
 
 async function fullPort(port) {
-  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan.json`]);
+  const writeStream = fs.createWriteStream('./ips1');
+  const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ -`]);
 
   childProcess.stdout.on('data', (data) => {
     // Process the output as needed
-    console.log(data.toString());
+    if (line.startsWith('{')) {
+      const obj = JSON.parse(line);
+      for (const port of obj.ports) {
+        if (port.reason !== "syn-ack") {
+          const splitIP = obj.ip.split('.');
+          const buffer = Buffer.from([
+            parseInt(splitIP[0]),
+            parseInt(splitIP[1]),
+            parseInt(splitIP[2]),
+            parseInt(splitIP[3]),
+            Math.floor(port.port / 256),
+            port.port % 256
+          ]);
+          writeStream.write(buffer);
+        }
+      }
+    } else {
+      console.log(data.toString());
+    }
   });
 
   childProcess.stderr.on('data', (data) => {
@@ -22,8 +41,8 @@ async function fullPort(port) {
   childProcess.on('close', async (code) => {
     if (code === 0) {
       console.log('Masscan finished.');
-      await save();
-      known24s();
+      writeStream.end();
+      //knownIps();
     } else {
       console.error(`Command exited with code ${code}`);
     }
