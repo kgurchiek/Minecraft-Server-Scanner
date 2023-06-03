9f5e90e07253aff271b5b1f9ccefe79093cefdd1
kgurchiek <kgurchiek1@gmail.com>
2023-06-03 09:40:31 -0700

    fullPort bug fixes

--- fullPort.js
+++ fullPort.js
@@ -6,10 +6,18 @@ async function fullPort(port) {
   const writeStream = fs.createWriteStream('./ips1');
   const childProcess = spawn('sh', ['-c', `sudo masscan -p ${port} 0.0.0.0/0 --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ -`]);
 
+  var leftOver = null;
   childProcess.stdout.on('data', (data) => {
-    // Process the output as needed
-    if (data.toString().startsWith('{')) {
-      const obj = JSON.parse(data.toString());
+    console.log('Data')
+    console.log(data.toString().split('\n,\n')[0] == '');
+    var string = data.toString();
+    if (leftOver == null) string = string.substring(string.indexOf('{'));
+    if (leftOver != null) string = leftOver + string;
+    console.log(`String: ${string.substring(0, 1)}`);
+    for (var i = 0; i < string.split('\n,\n').length - 1; i++) {
+      var line = string.split('\n,\n')[i];
+      if (line.startsWith('\n') || line.startsWith(',') == '') continue;
+      const obj = JSON.parse(line);
       for (const port of obj.ports) {
         if (port.reason !== "syn-ack") {
           const splitIP = obj.ip.split('.');
@@ -24,8 +32,26 @@ async function fullPort(port) {
           writeStream.write(buffer);
         }
       }
-    } else {
-      console.log(data.toString());
+      try {
+        const obj = JSON.parse(string.split('\n,\n')[string.split('\n,\n').length - 1]);
+        for (const port of obj.ports) {
+          if (port.reason !== "syn-ack") {
+            const splitIP = obj.ip.split('.');
+            const buffer = Buffer.from([
+              parseInt(splitIP[0]),
+              parseInt(splitIP[1]),
+              parseInt(splitIP[2]),
+              parseInt(splitIP[3]),
+              Math.floor(port.port / 256),
+              port.port % 256
+            ]);
+            writeStream.write(buffer);
+          }
+        }
+        leftOver = '';
+      } catch (err) {
+        leftOver = string.split('\n,\n')[string.split('\n,\n').length - 1];
+      }
     }
   });
 
@@ -45,4 +71,4 @@ async function fullPort(port) {
   });
 }
 
-fullPort(25500);
+fullPort(25565);
