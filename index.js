e485c43a51a64054466f78fa957012848cddcdc6
kgurchiek <kgurchiek1@gmail.com>
2023-06-04 07:34:24 -0700

    update known24s in index

--- index.js
+++ index.js
@@ -75,7 +75,9 @@ async function fullPort(port) {
 }
 
 async function known24s() {
-  ips = {};
+  // copy ips1
+  const writeStream = fs.createWriteStream('./ips2');
+
   fs.open('ips1', 'r', function(status, fd) {
     if (status) {
       console.log(status.message);
@@ -84,26 +86,78 @@ async function known24s() {
     const size = fs.statSync('ips1').size;
     var buffer = Buffer.alloc(size);
     fs.read(fd, buffer, 0, buffer.length, 0, function(err, num) {
-      for (var i = 0; i < buffer.length; i += 6) ips[`${buffer[i]}.${buffer[i + 1]}.${buffer[i + 2]}.0/24`] = 0;
+      writeStream.write(buffer);
+    })
+  })
+  
+  const ips = {};
+  const ipRanges = {};
+  fs.open('ips1', 'r', function(status, fd) {
+    if (status) {
+      console.log(status.message);
+      return;
+    }
+    const size = fs.statSync('ips1').size;
+    var buffer = Buffer.alloc(size);
+    fs.read(fd, buffer, 0, buffer.length, 0, function(err, num) {
+      console.log(`size: ${size}`);
 
-      fs.writeFile('./includeFile.txt', JSON.stringify(Object.keys(ips)).replaceAll('"', '').replaceAll('[', '').replaceAll(']', ''), function (err) {
+      for (var i = 0; i < buffer.length; i += 6) {
+        ips[`${buffer[i]}.${buffer[i + 1]}.${buffer[i + 2]}.${buffer[i + 3]}:${buffer[i + 4] * 256 + buffer[i + 5]}`] = 0;
+        ipRanges[`${buffer[i]}.${buffer[i + 1]}.${buffer[i + 2]}.0/24`] = 0;
+      }
+
+      fs.writeFile('./includeFile.txt', JSON.stringify(Object.keys(ipRanges)).replaceAll('"', '').replaceAll('[', '').replaceAll(']', ''), function (err) {
         if (err) console.error(err);
         const childProcess = spawn('sh', ['-c', `sudo masscan -p 25540-25700 --include-file includeFile.txt --rate=${config.packetLimit} --source-port 61000 --banners --excludefile ../masscan/data/exclude.conf -oJ masscan2.json`]);
 
+        var leftOver = null;
         childProcess.stdout.on('data', (data) => {
-          // Process the output as needed
-          console.log(data.toString());
+          var string = data.toString();
+          if (leftOver == null) string = string.substring(string.indexOf('{'));
+          if (leftOver != null) string = leftOver + string;
+          for (var i = 0; i < string.split('\n,\n').length - 1; i++) {
+            var line = string.split('\n,\n')[i];
+            try {
+              if (line.startsWith('[')) line = line.substring(1);
+              const obj = JSON.parse(line);
+              for (const port of obj.ports) {
+                if (port.reason !== "syn-ack") ips[`${obj.ip}:${port.port}`] = 0;
+              }
+              try {
+                const obj = JSON.parse(string.split('\n,\n')[string.split('\n,\n').length - 1]);
+                for (const port of obj.ports) {
+                  if (port.reason !== "syn-ack") ips[`${obj.ip}:${port.port}`] = 0;
+                }
+                leftOver = '';
+              } catch (err) {
+                leftOver = string.split('\n,\n')[string.split('\n,\n').length - 1];
+              }
+            } catch (err) {}
+          }
         });
 
         childProcess.stderr.on('data', (data) => {
-          // Handle any error output
           console.error(data.toString());
         });
 
         childProcess.on('close', async (code) => {
           if (code === 0) {
+            for (const ip of Object.keys(ips)) {
+              splitIP = ip.split(':')[0].split('.');
+              port = ip.split(':')[1];
+              const buffer = Buffer.from([
+                parseInt(splitIP[0]),
+                parseInt(splitIP[1]),
+                parseInt(splitIP[2]),
+                parseInt(splitIP[3]),
+                Math.floor(port / 256),
+                port % 256
+              ]);
+              writeStream.write(buffer);
+            }
             console.log('Masscan finished.');
-            await save2();
+            writeStream.end();
             knownIps();
           } else {
             console.error(`Command exited with code ${code}`);
