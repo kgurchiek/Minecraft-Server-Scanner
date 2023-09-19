3836ce39e45a95d28571b6b02cc64a2a828e47e4
Cornbread2100 <cornbread2100@cornbread2100.com>
2023-09-19 07:14:40 -0400

    Add path import

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { spawn } = require('child_process');
+const path = require('path');
 const minecraftCheck = require('./minecraftCheck.js');
 const config = require('./config.json');
 
